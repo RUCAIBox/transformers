cfb7595f95f978140aa936ba8c48751bdaa21bc1
Zhipeng Chen <zhipeng_chen@ruc.edu.cn>
2022-12-05 18:40:50 +0800

    FIX Fast generation

--- src/transformers/generation/beam_search.py
+++ src/transformers/generation/beam_search.py
@@ -387,6 +387,153 @@ class BeamSearchScorer(BeamScorer):
         )
 
 
+class BeamSearchScorerV2(BeamSearchScorer):
+
+    def process(
+        self,
+        input_ids: torch.LongTensor,
+        next_scores: torch.FloatTensor,
+        next_tokens: torch.LongTensor,
+        next_indices: torch.LongTensor,
+        pad_token_id: Optional[int] = None,
+        eos_token_id: Optional[int] = None,
+        beam_indices: Optional[torch.LongTensor] = None,
+    ) -> Tuple[torch.Tensor]:
+        if (beam_indices is not None):
+            warnings.warn("Fastseq do not support 'beam_indices' and it will be ignored.")
+        cur_len = input_ids.shape[-1]
+        batch_size = len(self._beam_hyps)
+        beams_offset = (torch.arange(0, batch_size) * self.num_beams).unsqueeze(1).type_as(input_ids)
+        cand_size = 2 * self.num_beams
+        cand_offsets = torch.arange(0, cand_size).type_as(input_ids)
+        if not (batch_size == (input_ids.shape[0] // self.group_size)):
+            if self.num_beam_groups > 1:
+                raise ValueError(
+                    f"A group beam size of {input_ids.shape[0]} is used as the input, but a group beam "
+                    f"size of {self.group_size} is expected by the beam scorer."
+                )
+            else:
+                raise ValueError(
+                    f"A beam size of {input_ids.shape[0]} is used as the input, but a beam size of "
+                    f"{self.group_size} is expected by the beam scorer."
+                )
+
+        effective_beam_id =  next_indices + beams_offset
+        if eos_token_id is not None:
+            eos_mask = next_tokens.eq(eos_token_id)
+        else: 
+            eos_mask = torch.zeros_like(next_tokens).bool()
+        eos_effective_idx = torch.masked_select(effective_beam_id[:, :self.num_beams], mask=eos_mask[:, :self.num_beams]
+        )
+        
+        eos_effective_scores = torch.masked_select(
+            next_scores[:, :self.num_beams], mask=eos_mask[:, :self.num_beams]
+        )
+        input_ids_cpu = input_ids.cpu()
+        eos_effective_idx_cpu= eos_effective_idx.cpu()
+        eos_effective_scores_cpu = eos_effective_scores.cpu()
+        for i in range (0, eos_effective_idx_cpu.size()[-1]):
+            # batch_idx = eos_effective_idx_cpu[i] // self.num_beams
+            batch_idx = torch.div(eos_effective_idx_cpu[i], self.num_beams, rounding_mode='floor')
+            if not self._done[batch_idx] :
+                self._beam_hyps[batch_idx.item()].add(
+                    input_ids_cpu[eos_effective_idx_cpu[i]].clone(),
+                    eos_effective_scores_cpu[i])
+            self._done[batch_idx] = (
+                self._done[batch_idx] or
+                self._beam_hyps[batch_idx].is_done(
+                next_scores[batch_idx].max().item(), cur_len))
+
+        active_mask = torch.add(eos_mask.type_as(cand_offsets) * cand_size, cand_offsets[:eos_mask.size(1)])
+        _, active_hypos = torch.topk(
+            active_mask, k=self.num_beams, dim=1, largest=False)
+        active_effective_beam_id  = torch.gather(
+            effective_beam_id, dim=1, index=active_hypos)
+        active_scores  = torch.gather(next_scores,
+            dim=1, index=active_hypos)
+        active_tokens  = torch.gather(next_tokens,
+            dim=1, index=active_hypos)
+        beam_idxs = active_effective_beam_id.view(-1)
+        beam_scores = active_scores.view(-1)
+        beam_tokens = active_tokens.view(-1)
+
+        return UserDict(
+            {
+                "next_beam_scores": beam_scores,
+                "next_beam_tokens": beam_tokens,
+                "next_beam_indices": beam_idxs
+            }
+        )
+    
+    def finalize(
+        self,
+        input_ids: torch.LongTensor,
+        final_beam_scores: torch.FloatTensor,
+        final_beam_tokens: torch.LongTensor,
+        final_beam_indices: torch.LongTensor,
+        max_length: int,
+        pad_token_id: Optional[int] = None,
+        eos_token_id: Optional[int] = None,
+        beam_indices: Optional[torch.LongTensor] = None,
+    ) -> Tuple[torch.LongTensor]:
+        if (beam_indices is not None):
+            warnings.warn("Fastseq do not support 'beam_indices' and it will be ignored.")
+
+        batch_size = len(self._beam_hyps)
+
+        # finalize all open beam hypotheses and add to generated hypotheses
+        for batch_idx, beam_hyp in enumerate(self._beam_hyps):
+            if self._done[batch_idx]:
+                continue
+
+            # all open beam hypotheses are added to the beam hypothesis
+            # beam hypothesis class automatically keeps the best beams
+            for beam_id in range(self.num_beams):
+                batch_beam_idx = batch_idx * self.num_beams + beam_id
+                final_score = final_beam_scores[batch_beam_idx].item()
+                final_tokens = input_ids[batch_beam_idx]
+                beam_hyp.add(final_tokens, final_score)
+
+        batch_size = len(self._done)
+
+        # select the best hypotheses
+        sent_lengths = input_ids.new(batch_size * self.num_beam_hyps_to_keep)
+        best = []
+        best_scores = torch.zeros(batch_size * self.num_beam_hyps_to_keep, device=self.device, dtype=torch.float32)
+
+        # retrieve best hypotheses
+        for i, beam_hyp in enumerate(self._beam_hyps):
+            sorted_hyps = sorted(beam_hyp.beams, key=lambda x: x[0])
+            for j in range(self.num_beam_hyps_to_keep):
+                best_hyp_tuple = sorted_hyps.pop()
+                best_score = best_hyp_tuple[0]
+                best_hyp = best_hyp_tuple[1]
+                sent_lengths[self.num_beam_hyps_to_keep * i + j] = len(best_hyp)
+
+                # append to lists
+                best.append(best_hyp)
+                best_scores[i * self.num_beam_hyps_to_keep + j] = best_score
+
+        # prepare for adding eos
+        sent_max_len = min(sent_lengths.max().item() + 1, max_length)
+        decoded: torch.LongTensor = input_ids.new(batch_size * self.num_beam_hyps_to_keep, sent_max_len)
+        # shorter batches are padded if needed
+        if sent_lengths.min().item() != sent_lengths.max().item():
+            assert pad_token_id is not None, "`pad_token_id` has to be defined"
+            decoded.fill_(pad_token_id)
+
+        # fill with hypotheses and eos_token_id if the latter fits in
+        for i, hypo in enumerate(best):
+            decoded[i, : sent_lengths[i]] = hypo
+            if sent_lengths[i] < max_length:
+                decoded[i, sent_lengths[i]] = eos_token_id
+        return UserDict(
+            {
+                "sequences": decoded,
+                "sequence_scores": best_scores,
+            }
+        )
+
 class ConstrainedBeamSearchScorer(BeamScorer):
     r"""
     [`BeamScorer`] implementing constrained beam search decoding.
