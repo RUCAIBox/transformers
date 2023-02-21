3225799e0ac5e106592723e8301dc4e04b3b8d1d
yu ge <yuge375841@ruc.edu.cn>
2023-02-21 21:52:58 +0800

    add lora module

--- src/transformers/adapter.py
+++ src/transformers/adapter.py
@@ -75,3 +75,67 @@ class Adapter(nn.Module):
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         return input + self.adapter(input)
+    
+class LoRAMergedLinear(nn.Linear):
+
+    def __init__(self, in_features, out_features, config):
+        super().__init__(in_features, out_features)
+        self.r = config.lora_r
+        self.alpha = config.lora_alpha
+        self.scaling = self.alpha / self.r
+        # self.enable_lora= self.enable_lora
+        # self.lora_dropout=config.lora_dropout
+        self.enable_lora= [True, False, True]
+        self.lora_A = nn.Parameter(
+            self.weight.new_zeros((config.lora_r * sum(self.enable_lora), in_features)))
+        self.lora_B = nn.Parameter(
+            self.weight.new_zeros((out_features // len(self.enable_lora) * sum(self.enable_lora), config.lora_r))
+        )  # weights for Conv1D with groups=sum(enable_lora)
+        self.lora_ind = self.weight.new_zeros(
+            (out_features,), dtype=torch.bool
+        ).view(len(self.enable_lora), -1) #torch.Size([3, 768])
+        self.lora_ind[self.enable_lora, :] = True
+        self.lora_ind = self.lora_ind.view(-1) #变成一个torch.Size([2304])形状的tensor，[ True,  True,  True, False, False, False,  True,  True,  True]
+        self.lora_dropout = nn.Dropout(p=config.lora_dropout)
+        self.my_reset_parameters()
+        self.weight.requires_grad = False # Freezing the pre-trained weight matrix
+        self.weight.data = self.weight.data.T
+
+    def my_reset_parameters(self):
+        # nn.Linear.reset_parameters(self) #没用了
+        # if hasattr(self, 'lora_A'):
+            # initialize A the same way as the default for nn.Linear and B to zero
+        nn.init.kaiming_uniform_(self.lora_A, a=math.sqrt(5))
+        nn.init.zeros_(self.lora_B)
+
+    def zero_pad(self, x): #中间列pad一半的0
+        result = x.new_zeros((*x.shape[:-1], self.out_features)) #相当于把x的最后一个维度换成out features，生成一个全0的tensor
+        result = result.view(-1, self.out_features)
+        # print(self.lora_ind)
+        # print(self.out_features) #2304
+        # print(self.lora_ind.shape) #torch.Size([2304])
+        # print(result.shape,x.shape) #torch.Size([202, 2304]) torch.Size([1, 202, 1536])
+        # print(x.reshape(-1,self.out_features//len(self.enable_lora)*sum(self.enable_lora)).shape)
+        # return
+        result[:, self.lora_ind] = x.reshape(
+            -1, self.out_features // len(self.enable_lora) * sum(self.enable_lora)
+        )
+        return result.view((*x.shape[:-1], self.out_features))
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        
+        result = F.linear(input, self.weight.T, bias=self.bias)
+        # print(result.shape) #torch.Size([1, 202, 2304])
+        after_A = F.linear(self.lora_dropout(input), self.lora_A) 
+        # print(input.shape) #torch.Size([1, 202, 768])
+        # print(after_A.shape) #torch.Size([1, 202, 8])
+        # print(after_A.transpose(-2, -1).shape) #torch.Size([1, 8, 202])
+        # print(self.lora_A.shape,self.lora_B.shape) #torch.Size([8, 768]) torch.Size([1536, 4])
+        # print(self.lora_B.unsqueeze(-1).shape) #torch.Size([1536, 4, 1])
+        after_B = F.conv1d( #torch.Size([1, 202, 1536])
+            after_A.transpose(-2, -1), #torch.Size([1, 8, 202]) ——(Batch_Size,In_Channel,Length)
+            self.lora_B.unsqueeze(-1), #torch.Size([1536, 4, 1]) ——(Out_Channel,In_Channel/Group,Kernel_Size)
+            groups=sum(self.enable_lora) #2
+        ).transpose(-2, -1)
+        result += self.zero_pad(after_B) * self.scaling
+        return result            
